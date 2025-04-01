3c7adb828e63913d8879a98f368d4113667b69da
Hasan Sultan <52581466+hasansultan92@users.noreply.github.com>
2025-04-01 13:44:24 -0400

    Update install_libcachesim.sh

--- scripts/install_libcachesim.sh
+++ scripts/install_libcachesim.sh
@@ -8,7 +8,7 @@ DIR=$(dirname ${SORUCE})
 cd ${DIR}/../;
 mkdir _build || true 2>/dev/null;
 cd _build;
-cmake ..;
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug ..;
 make -j;
 cd ${DIR}; 
 
